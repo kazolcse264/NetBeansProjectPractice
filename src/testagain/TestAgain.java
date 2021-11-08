3b523dbdc05da5085ac9adfd405960cc4b77f058
Md Kazol Biswas <74205867+kazolcse264@users.noreply.github.com>
2021-11-08 15:41:35 +0600

    Multiple Inheritance is occurred.

--- src/testagain/TestAgain.java
+++ src/testagain/TestAgain.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package testagain;
 
-/**
- *
- * @author HP
- */
+interface Backend {
+
+    // abstract class
+    public void connectServer();
+}
+
 public class TestAgain {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Test Again.");
+
+        Language java = new Language();
+
+        java.connectServer();
+
+        // call the inherited method of Frontend class
+        java.responsive(java.language);
+
     }
 
+}
+
+class Frontend {
+
+    public void responsive(String str) {
+        System.out.println(str + " can also be used as frontend.");
+    }
+}
+
+// Language extends Frontend class
+// Language implements Backend interface
+class Language extends Frontend implements Backend {
+
+    String language = "Java";
+
+    // implement method of interface
+    public void connectServer() {
+        System.out.println(language + " can be used as backend language.");
+    }
 }
